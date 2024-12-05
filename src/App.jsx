78cf2690a7b45ec1bd435c918193e453852458d9
unknown <global-greategumamhe@gmail.com>
2024-12-05 09:51:30 +0100

    Figma Application

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import React from 'react'
+import Navbar from './Components/navbar'
+import { Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+
+
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+      <Route path='/about' element={ <About /> } />
+      <Route path='/product' element={ <Product /> } />
+      <Route path='/contact' element={ <Contact /> } />
+
+      
+
+    </Routes>
+    </div>
+  )
+};
+
+export default App
